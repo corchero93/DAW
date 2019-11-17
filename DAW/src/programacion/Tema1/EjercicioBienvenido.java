614ca142de83aae7ea518e0720a37cecd0bbf283
corchero93 <snake@192.168.1.48>
2019-11-17 21:52:23 +0100

    Ejercicio Bienvenido

---
+++ DAW/src/programacion/Tema1/EjercicioBienvenido.java
@@ -0,0 +1,11 @@
+package programacion.Tema1;
+
+public class EjercicioBienvenido
+{
+	public static void main(String[] args)
+	{
+	System.out.print("Bienvenido a la programaciòn en java");
+	}
+
+}
+
